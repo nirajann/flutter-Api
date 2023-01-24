1e78313d284a3135b4e74660013aa9a257b7e2d0
Nirajan gautam <78842151+nirajann@users.noreply.github.com>
2023-01-24 11:51:53 +0545

    all

---
+++ lib/model/batch.dart
@@ -0,0 +1,35 @@
+import 'package:batch_student_objbox_api/model/student.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:objectbox/objectbox.dart';
+
+//flutter pub run build_runner build  --delete-conflicting-outputs
+
+@Entity()
+@JsonSerializable()
+class Batch {
+  @Id(assignable: true)
+  int id;
+
+  @Unique()
+  @JsonKey(name: '_id')
+  String? batchId;
+  String? batchName;
+
+  @Backlink()
+  final student = ToMany<Student>();
+
+  //Constructor
+  Batch(this.batchId, this.batchName, {this.id = 0});
+
+  factory Batch.fromJson(Map<String, dynamic> json) {
+    return Batch(
+      json['_id'],
+      json['batchName'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': batchId,
+        'batchName': batchName,
+      };
+}
